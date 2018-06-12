e2abd0b8c67c4ff2f71396fb98ba1525ce3f8f84
avinashbhosale998 <avinashbhosale998@gmail.com>
2018-06-12 22:43:11 +0530

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import CharacterList from './CharacterList';
+
+export default class App extends Component {
+    render() {
+        return(
+            <div>
+                <h2>SuperSquad</h2>
+                <CharacterList />
+            </div>
+        );
+    }
+}
